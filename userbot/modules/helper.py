e1f533a5d5cbe3742dd9b4504a11840df71e02d8
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-02-04 00:59:30 +0700

    Update helper.py

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -13,10 +13,10 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Hai {DEFAULTUSER} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        f"✣ **Group Support :** [UNREALBABIES](t.me/UnrealBabies)\n"
-        f"✣ **Channel :** [Zelda Projects](t.me/zldprojects)\n"
-        f"✣ **Owner :** [Lord Zelda](t.me/UnrealZlda)\n"
-        f"✣ **Repo :** [ZELDA USERBOT](https://github.com/nmiabdfhmy/Zelda-Ubot)\n"
+        f"✣ **Group Support :** [ZONEDANGERSEX](t.me/ZoneDangerSex)\n"
+        f"✣ **Channel :** [Revan Store](t.me/Revanstoreya)\n"
+        f"✣ **Owner :** [Lord Revans](t.me/Revans505)\n"
+        f"✣ **Repo :** [Revans USERBOT](https://github.com/Rehansaputradewantoro/Han-Ubot)\n"
     )
 
 
@@ -24,7 +24,7 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://telegra.ph/List-Variabel-Heroku-untuk-ZELDA USERBOT-09-22)"
+        "\n[DAFTAR VARS](https://telegra.ph/List-Variabel-Heroku-untuk-Revans USERBOT-09-22)"
     )
 
 
@@ -32,13 +32,13 @@ CMD_HELP.update(
     {
         "helper": f"**Plugin : **`helper`\
         \n\n  •  **Syntax :** `{cmd}ihelp`\
-        \n  •  **Function : **Bantuan Untuk ZELDA USERBOT.\
+        \n  •  **Function : **Bantuan Untuk REVANS USERBOT.\
         \n\n  •  **Syntax :** `{cmd}listvar`\
         \n  •  **Function : **Melihat Daftar Vars.\
         \n\n  •  **Syntax :** `{cmd}repo`\
-        \n  •  **Function : **Melihat Repository ZELDA USERBOT.\
+        \n  •  **Function : **Melihat Repository REVANS USERBOT.\
         \n\n  •  **Syntax :** `{cmd}string`\
-        \n  •  **Function : **Link untuk mengambil String ZELDA USERBOT.\
+        \n  •  **Function : **Link untuk mengambil String REVANS USERBOT.\
     "
     }
 )
